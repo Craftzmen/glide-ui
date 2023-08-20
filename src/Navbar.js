cb1c91e2ed22b5009d32a916eae39b15b94271a6
Craftzmen <abdullahimran30.9e@gmail.com>
2023-08-20 21:14:58 +0500

    chore(docs): panel + layout refactoring

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = (props) => {
     return (
-        <Box {...props} display={'flex'} width={'100%'} px={10} py={3.5} borderBottom={'1px'} borderColor={'gray.200'} backgroundColor={'white'} position={'sticky'} top={'0'} >
+        <Box {...props} zIndex={10} display={'flex'} width={'100%'} px={10} py={3.5} borderBottom={'1px'} borderColor={'gray.200'} backgroundColor={'white'} position={'sticky'} top={'0'} >
             <Link to='/' >
                 <Text fontFamily={'sans-serif'} fontWeight={'bold'} fontSize={'lg'}  ><Box fontSize={'xl'} display={'inline'} >#</Box> glide/ui</Text>
             </Link>
